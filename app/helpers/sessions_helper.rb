22379b829c27970f84ca63f9bdc81c094285bb56
JMSilla <josilma1@gmail.com>
2012-04-15 13:16:29 +0100

    Finished unit 9 exercises

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    session.delete(:remember_token)
     self.current_user = nil
   end
 
@@ -28,6 +28,6 @@ module SessionsHelper
   end
 
   def remember_token
-    cookies.signed[:remember_token] || [nil, nil]
+    session[:remember_token] || [nil, nil]
   end
 end
